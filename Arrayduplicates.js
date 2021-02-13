acddcd76607f096df2f58134f7ab9ea0e8e3ae69
viky28 <viky.pu28@gmail.com>
2021-02-13 11:36:58 +0530

    Update Arrayduplicates.js

--- Arrayduplicates.js
+++ Arrayduplicates.js
@@ -13,3 +13,32 @@ function uniqueArray(array){
 }
 uniqueArray(array1);
 console.log(uniqueArray(array1))
+const arr = [12,14,20,21,28,12,23,14,21];
+
+let unique1 = arr.filter((res,index)=>{
+  return arr.indexOf(res) === index;
+})
+console.log(unique1)
+// method 1------
+
+const unique = [...new Set(arr)]
+console.log(unique)
+
+// method 2-------
+function removeDuplicates(array){
+  const result = [];
+  const obj = {};
+  for(let i=0;i<array.length;i++){
+    if(obj[array[i]]){
+      console.log(obj)
+      continue;
+    } else {
+      result.push(array[i])
+      obj[array[i]] = true;
+      console.log(obj)
+    }
+  }
+  return result
+}
+console.log(removeDuplicates(arr))
+
